d67612c236fbf8f8f8392a14c087ae9b4af38d66
DiverCesar <diver.cesar123@gmail.com>
2026-04-27 01:01:19 -0500

    Update try catch

--- db.php
+++ db.php
@@ -3,13 +3,14 @@ $host = 'db.byixbngbbdyugffpnylx.supabase.co';
 $port = '5432';
 $dbname = 'postgres';
 $user = 'postgres';
-$password = 'Pufle.landia15';
+$password = 'Pufle.landia15'; 
 
 try {
     $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
     $pdo = new PDO($dsn, $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Database connection failed.");
+    // Esto imprimirá el error real en la pantalla de Railway
+    die("Error de conexión PDO: " . $e->getMessage());
 }
 ?>
