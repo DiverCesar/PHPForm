265ffbd860e16dac19f2009ba1b654653892c6dd
DiverCesar <diver.cesar123@gmail.com>
2026-04-27 01:26:26 -0500

    Changing user IPV4

--- db.php
+++ db.php
@@ -1,16 +1,15 @@
 <?php
-$host = 'db.byixbngbbdyugffpnylx.supabase.co';
+$host = 'aws-1-us-east-1.pooler.supabase.com';
 $port = '5432';
 $dbname = 'postgres';
-$user = 'postgres';
-$password = 'Pufle.landia15'; 
+$user = 'postgres.byixbngbbdyugffpnylx';
+$password = 'Pufle.landia15';
 
 try {
     $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
     $pdo = new PDO($dsn, $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    // Esto imprimirá el error real en la pantalla de Railway
     die("Error de conexión PDO: " . $e->getMessage());
 }
 ?>
